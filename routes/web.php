7e8b4fb794da6860b713f4939c1d9cbf3c783699
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-03 16:24:04 +0900

    Fix: ルーティング練習

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('hello/{msg?}', function ($msg='no message.') {
+  $html = <<<EOF
+  <html>
+  <head>
+  <title>hello</title>
+  <style>
+  body {font-size: 16pt; color: #999; }
+  h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
+  </style>
+  </head>
+  <body>
+    <h1>hello</h1>
+    <p>{$msg}</p>
+    <p>これは、サンプルで作ったページです。</p>
+  </body>
+  </html>
+  EOF;
+
+    return $html;
 });
