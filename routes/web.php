7c8d80279f00982221cf2e473365e83e9fcb1f4f
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-03 17:02:03 +0900

    Fix: HelloController中身記述とルート変更

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,4 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello/{msg?}', function ($msg='no message.') {
-  $html = <<<EOF
-  <html>
-  <head>
-  <title>hello</title>
-  <style>
-  body {font-size: 16pt; color: #999; }
-  h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-  </style>
-  </head>
-  <body>
-    <h1>hello</h1>
-    <p>{$msg}</p>
-    <p>これは、サンプルで作ったページです。</p>
-  </body>
-  </html>
-  EOF;
-
-    return $html;
-});
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
