68dd4a96ccb3ebc28e75f2de7fda3422c37946f1
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-04 12:11:32 +0900

    Fix: クエリー文字列からviewに値を渡す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\Redis;
 
 class HelloController extends Controller
 {
-  public function index($id='zero') {
+  public function index(Request $request) {
     $data = [
       'msg'=>'これはコントローラから渡されたメッセージだよ',
-      'id'=>$id
+      'id'=>$request->id
     ];
     return view('hello.index', $data);
   }
