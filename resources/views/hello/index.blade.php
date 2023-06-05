fb7cbe892e731037a405aca8e9eec628920d6cd4
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-05 20:23:21 +0900

    Add: コンポーネントとしてmessage.blade.phpを作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,15 @@
 @section('content')
   <p>ここが本文のコンテンツだよ</p>
   <p>必要なだけ書けるよ</p>
+  @component('components.message')
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+
+    @slot('msg_content')
+    これはメッセージの表示だよ
+    @endslot
+  @endcomponent
 @endsection
 
 @section('footer')
