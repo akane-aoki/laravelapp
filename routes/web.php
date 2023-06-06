cdf72954594239c239ec5f83c166caa347fc6430
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-06 17:11:43 +0900

    Add: HelloMiddlewareを作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\HelloMiddleware;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,5 +17,5 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
-Route::post('hello', 'App\Http\Controllers\HelloController@post');
+Route::get('hello', 'App\Http\Controllers\HelloController@index')
+  ->middleware(HelloMiddleware::class);
