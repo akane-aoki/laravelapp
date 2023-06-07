c32f9d7e7fbd99cbf400faefce4ccc7ba69c2138
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-06-07 10:43:09 +0900

    Fix: バリデーションの作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,16 @@
 @endsection
 
 @section('content')
-  <p>ここが本文のコンテンツだよ</p>
-  <p>これは、<middleware>google.com</middleware>へのリンクだよ</p>
-  <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクだよ</p>
+  <p>{{$msg}}</p>
+  <form action="/hello" method="post">
+  <table>
+    @csrf
+    <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+    <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
+    <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
+  </table>
+  </form>
 @endsection
 
 @section('footer')
